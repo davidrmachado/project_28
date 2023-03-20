e4006c4e1d41c16b43296181aab46f383a08fa95
David-Neto <davidrmachado@gmail.com>
2023-03-20 20:07:44 -0300

    Entrega do requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,12 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    all_news = search_news({"title": {"$regex": title, "$options": "i"}})
+    result = [(news["title"], news["url"]) for news in all_news]
+
+    return result
 
 
 # Requisito 8
